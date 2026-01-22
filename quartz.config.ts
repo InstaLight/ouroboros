f1bcc55d659958e2de236f6a1638f2a2ce08b8fe
InstaLight <patricksmithinbox@gmail.com>
2026-01-22 13:29:09 -0500

    Quartz sync: Jan 22, 2026, 1:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Ouroboros Wiki",
+    pageTitleSuffix: "A unnecessarily large universe written by InstaLight",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ouroboros.instalight.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
